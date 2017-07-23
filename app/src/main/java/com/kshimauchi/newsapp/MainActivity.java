6cac797eaa9f9dc14ef47f5dba3e3cc67fd43c07
“kshimauchi” <kshimauchi@gmail.com>
2017-07-23 16:59:21 -0700

    Sixth Commit

--- app/src/main/java/com/kshimauchi/newsapp/MainActivity.java
+++ app/src/main/java/com/kshimauchi/newsapp/MainActivity.java
@@ -1,7 +1,12 @@
 package com.kshimauchi.newsapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
 import android.support.v4.content.Loader;
@@ -12,41 +17,46 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.ProgressBar;
 
-import com.kshimauchi.newsapp.Model.NewsItem;
+import com.kshimauchi.newsapp.data.Contract;
+import com.kshimauchi.newsapp.data.DatabaseHelper;
+import com.kshimauchi.newsapp.data.DatabaseUtils;
+//line 81 and line 41
+//implementing Loadermanager with loadercallbacks is part of the routine used in swapping out Asysntask with AsyntaskLoader
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Void>,
+        NewsAdapter.ItemClickListener {
 
-import org.json.JSONException;
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.ArrayList;
-
-import static com.kshimauchi.newsapp.NewsAdapter.ItemClickListener;
-
-//
-
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<NewsItem>> {
     static final String TAG = "MainActivity";
     private ProgressBar progress;
-    private EditText search;
     private RecyclerView recView;
-    public final static String URLKEY = "url";
-    //maybe unnecessary in subsequent builds here it may not be implemented
-    private static final int Search_Loader = 1;
-    public final static String SEARCH_QUERY_EXTRA = "";
-
+    /**Adapter, Cursor, SQLLite Database **/
+    private NewsAdapter adapter;
+    private Cursor cursor;
+    private SQLiteDatabase db;
+    private static final int NEWS_LOADER =1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //check this
+
         setContentView(R.layout.activity_main);
 
         progress = (ProgressBar) findViewById(R.id.progressBar);
-        search = (EditText) findViewById(R.id.searchQuery);
+        //search = (EditText) findViewById(R.id.searchQuery);  removed in homework4 as per request it is not needed
         recView = (RecyclerView) findViewById(R.id.recyclerView);
         recView.setLayoutManager(new LinearLayoutManager(this));
+
+        //allows one to store data in key/value pairs
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isFirst = preferences.getBoolean("isFirst", true);
+
+        if(isFirst){
+           load();
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("isFirst", false);
+            editor.apply();
+        }
+       ScheduleUtilities.scheduleRefresh(this);
     }
 
     @Override
@@ -60,81 +70,132 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         int itemNumber = item.getItemId();
         //search is the id in menu.xml
         if (itemNumber == R.id.search) {
-            String query = search.getText().toString();
-            //bundle creation bundles have 3 parameters
-            Bundle bundleQuery = new Bundle();
-            bundleQuery.putString(SEARCH_QUERY_EXTRA, query);
-            android.support.v4.app.LoaderManager loaderManager = getSupportLoaderManager();
-            android.support.v4.content.Loader<Object> loader = loaderManager.getLoader(Search_Loader);
-
-            if (loader == null) {
-                loaderManager.initLoader(Search_Loader, bundleQuery, this).forceLoad();
-
-            } else {
-                loaderManager.restartLoader(Search_Loader, bundleQuery, this);
-            }
+            load();
         }
         return true;
     }
 
+    public void load(){
+        LoaderManager ldManager = getSupportLoaderManager();
+        ldManager.restartLoader(NEWS_LOADER,null, this).forceLoad();
+//        adapter.notifyDataSetChanged();
+    }
+
     @Override
-    public Loader<ArrayList<NewsItem>> onCreateLoader(int id, final Bundle args) {
-        return new AsyncTaskLoader<ArrayList<NewsItem>>(this) {
+    public Loader<Void> onCreateLoader(int id, Bundle args) {
+        return new AsyncTaskLoader<Void>(this) {
             @Override
             protected void onStartLoading() {
                 super.onStartLoading();
-                if (args == null) return;
                 progress.setVisibility(View.VISIBLE);
             }
 
             @Override
-            public ArrayList<NewsItem> loadInBackground() {
-                //
-                String qry = args.getString(String.valueOf(search));
-                //if (qry == null || TextUtils.isEmpty(qry)) return
-                ArrayList<NewsItem> result = null;
-                //not passing in a query here because I have completely built the url a better approach
-                //would be to pass the API KEY
-                //44f7c25a59fc4280b594c6c32743cca1
-                URL url = NetworkUtils.buildURL(qry);
-                Log.d(TAG, "url :" + url.toString());
-                try {
-                    String json = NetworkUtils.getResponseFromHttpURL(url);
-                    result = NetworkUtils.parseJSON(json);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    return null;
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-                return result;
+            public Void loadInBackground() {
+                super.onStartLoading();
+                progress.setVisibility(View.VISIBLE);
+                return null;
             }
         };
     }
-
-    @Override
-    public void onLoadFinished(Loader<ArrayList<NewsItem>> loader, final ArrayList<NewsItem> data) {
-        progress.setVisibility(View.GONE);
-        if (data != null) {
-            NewsAdapter adapter = new NewsAdapter(data, new ItemClickListener() {
-
-                @Override
-                public void onItemClick(int clickedItemIndex) {
-                    String url = data.get(clickedItemIndex).getUrl();
-                    String.format("Url%s", url);
-                    Log.d("formated: url", url);
-                    //makes an intent without URI parse
-                    Intent browserIntent = new Intent(MainActivity.this, Web.class);
-                    browserIntent.putExtra(URLKEY, url);
-                    startActivity(browserIntent);
-                }
-            });
+        @Override
+        public void onLoadFinished (Loader < Void > loader, Void data){
+            progress.setVisibility(View.GONE); //removes the progress bar from being seen after it has retrieved data
+            db = new DatabaseHelper(MainActivity.this).getWritableDatabase();  //opens the datbase to used for reading and writing
+            cursor = DatabaseUtils.getAll(db);
+            adapter = new NewsAdapter(cursor, this);
             recView.setAdapter(adapter);
         }
-    }
-    //OnLoader Reset is not being used right now!
+
+        @Override
+        public void onLoaderReset (Loader < Void > loader) {    }
+
+        @Override
+        public void onItemClick (Cursor cursor,int clickedItemIndex){
+            cursor.moveToPosition(clickedItemIndex);
+            String url = cursor.getString(cursor.getColumnIndex(Contract.TABLE_ARTICLES.COLUMN_NAME_URL));
+            String.format("Url %s" + url);
+
+            Log.d(TAG, url);
+            //intent for the url
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW);
+            browserIntent.setData(Uri.parse(url));
+            startActivity(browserIntent);
+        }
     @Override
-    public void onLoaderReset(Loader<ArrayList<NewsItem>> loader) {
+    protected void onStart(){
+        super.onStart();
+        db = new DatabaseHelper(MainActivity.this).getWritableDatabase();
+        cursor = DatabaseUtils.getAll(db);
+        adapter = new NewsAdapter(cursor, this);
+        recView.setAdapter(adapter);
     }
+        @Override
+        protected void onStop(){
+            super.onStop();
+            db.close();
+            cursor.close();
+        }
+    }
+/**
+ *     @Override
+public Loader<ArrayList<NewsItem>> onCreateLoader(int id, final Bundle args) {
+return new AsyncTaskLoader<ArrayList<NewsItem>>(this) {
+@Override
+protected void onStartLoading() {
+super.onStartLoading();
+//    if (args == null) return;
+progress.setVisibility(View.VISIBLE);
 }
+
+@Override
+public ArrayList<NewsItem> loadInBackground() {  //removing ArrayList<NewsItem>
+
+String qry = args.getString(String.valueOf(""));
+//if (qry == null || TextUtils.isEmpty(qry)) return
+ArrayList<NewsItem> result = null;
+//not passing in a query here because I have completely built the url a better approach
+//would be to pass the API KEY
+//44f7c25a59fc4280b594c6c32743cca1
+URL url = NetworkUtils.buildURL(qry);
+Log.d(TAG, "url :" + url.toString());
+try {
+String json = NetworkUtils.getResponseFromHttpURL(url);
+result = NetworkUtils.parseJSON(json);
+} catch (IOException e) {
+e.printStackTrace();
+return null;
+} catch (JSONException e) {
+e.printStackTrace();
+return null;
+}
+return result;
+}
+};
+}
+
+
+ @Override
+ public void onLoadFinished(Loader<ArrayList<NewsItem>> loader, final ArrayList<NewsItem> data) {
+ progress.setVisibility(View.GONE);
+ if (data != null) {
+ NewsAdapter adapter = new NewsAdapter(data, new ItemClickListener() {
+
+ @Override
+ public void onItemClick(int clickedItemIndex) {
+ String url = data.get(clickedItemIndex).getUrl();
+ String.format("Url%s", url);
+ Log.d("formated: url", url);
+ //makes an intent without URI parse
+ Intent browserIntent = new Intent(MainActivity.this, Web.class);
+ browserIntent.putExtra(URLKEY, url);
+ startActivity(browserIntent);
+ }
+ });
+ recView.setAdapter(adapter);
+ }
+ }
+ *
+ *
+ *
+ * */
